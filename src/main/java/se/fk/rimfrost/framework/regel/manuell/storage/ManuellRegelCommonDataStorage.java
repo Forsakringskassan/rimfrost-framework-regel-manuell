356df12d9c0b49080b97a855c19cc5cb6b2726f1
Lars Persson <254442132+larpersso@users.noreply.github.com>
2026-03-06 08:53:43 +0100

    fix: Use interface-based storage for common rule data

---
+++ src/main/java/se/fk/rimfrost/framework/regel/manuell/storage/ManuellRegelCommonDataStorage.java
@@ -0,0 +1,14 @@
+package se.fk.rimfrost.framework.regel.manuell.storage;
+
+import se.fk.rimfrost.framework.regel.manuell.storage.entity.ManuellRegelCommonData;
+
+import java.util.UUID;
+
+public interface ManuellRegelCommonDataStorage
+{
+   ManuellRegelCommonData getManuellRegelCommonData(UUID handlaggningId);
+
+   void setManuellRegelCommonData(UUID handlaggningId, ManuellRegelCommonData manuellRegelCommonData);
+
+   void deleteManuellRegelCommonData(UUID handlaggningId);
+}
--- src/test/java/se/fk/rimfrost/framework/regel/manuell/storage/RegelManuellTestDataStorage.java
+++
@@ -1,7 +0,0 @@
-package se.fk.rimfrost.framework.regel.manuell.storage;
-
-import se.fk.rimfrost.framework.regel.manuell.storage.entity.RegelManuellDataStorage;
-
-public class RegelManuellTestDataStorage extends RegelManuellDataStorage
-{
-}
