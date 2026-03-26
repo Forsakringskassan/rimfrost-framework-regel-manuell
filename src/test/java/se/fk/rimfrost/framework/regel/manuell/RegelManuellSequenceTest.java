e03f48e931e760f406f6f30b4eee0c715c31318a
Ulf Slunga <ulf.slunga@sinetiq.se>
2026-03-26 07:53:27 +0100

    fix: refactoring av testerna

---
+++ src/test/java/se/fk/rimfrost/framework/regel/manuell/RegelManuellSequenceTest.java
@@ -0,0 +1,71 @@
+package se.fk.rimfrost.framework.regel.manuell;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import se.fk.rimfrost.Status;
+import se.fk.rimfrost.framework.regel.Utfall;
+import se.fk.rimfrost.jaxrsspec.controllers.generatedsource.model.UppgiftStatus;
+
+@QuarkusTest
+@QuarkusTestResource.List(
+{
+      @QuarkusTestResource(WireMockTestResource.class)
+})
+public class RegelManuellSequenceTest extends AbstractRegelManuellTest
+{
+
+   @ParameterizedTest
+   @CsvSource(
+   {
+         "5367f6b8-cc4a-11f0-8de9-199901011234, JA, 383cc515-4c55-479b-a96b-244734ef1336, 11e53b18-e9ac-4707-825b-a1cb80689c29",
+         "5367f6b8-cc4a-11f0-8de9-199901011234, NEJ , 383cc515-4c55-479b-a96b-244734ef1336, 11e53b18-e9ac-4707-825b-a1cb80689c29"
+   })
+   void TestRegelManuellSequenceTest(String handlaggningId, Utfall expectedUtfall, String utforarId, String uppgiftId)
+         throws Exception
+   {
+      System.out.printf("Starting TestRegelManuell. %S%n", handlaggningId);
+      resetState();
+
+      //
+      // Workflow start
+      //
+      sendRegelRequest(handlaggningId);
+      verifyGetHandlaggningProduced(handlaggningId);
+      verifyOulRequestProduced();
+
+      // Clear previous requests
+      wiremockServer.resetRequests();
+
+      //
+      // Send mocked OUL response
+      //
+      simulateOulResponse(handlaggningId, uppgiftId);
+
+      //
+      // Status update from OUL:
+      // verify that rule updates uppgift status in new PUT handlaggning
+      //
+      simulateOulStatus(handlaggningId, uppgiftId, utforarId, Status.NY);
+      verifyPutHandlaggningProduced(handlaggningId);
+
+      //
+      // Verify utökad uppgiftsbeskrivning
+      //
+      verifyUtokadUppgiftsbeskrivningProduced();
+
+      //
+      // Verify handläggning DONE
+      //
+      wiremockServer.resetRequests();
+      mockRegelService(expectedUtfall, handlaggningId);
+      sendPostRegelManuellHandlaggningDone(handlaggningId);
+      verifyPutHandlaggningContentUppgiftStatus(handlaggningId, utforarId, UppgiftStatus.AVSLUTAD);
+      verifyOulStatusMessageProduced();
+      verifyRegelResponseProduced();
+
+      verifyMocks(handlaggningId);
+   }
+
+}
