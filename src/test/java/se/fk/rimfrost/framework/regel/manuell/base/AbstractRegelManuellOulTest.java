cac29a0764807dcabc69232039c433d0de0506c2
Ulf Slunga <ulf.slunga@sinetiq.se>
2026-04-23 14:14:27 +0200

    fix: lägger till nytt OUL test

--- src/test/java/se/fk/rimfrost/framework/regel/manuell/base/AbstractRegelManuellOulTest.java
+++ src/test/java/se/fk/rimfrost/framework/regel/manuell/base/AbstractRegelManuellOulTest.java
@@ -1,10 +1,14 @@
 package se.fk.rimfrost.framework.regel.manuell.base;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import se.fk.rimfrost.Status;
+import se.fk.rimfrost.framework.oul.logic.dto.ImmutableIdtyp;
+import se.fk.rimfrost.framework.regel.manuell.helpers.WireMockRegelManuell;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Disabled("Base test class - not executable")
 public abstract class AbstractRegelManuellOulTest extends AbstractRegelManuellTest
@@ -41,6 +45,36 @@ public abstract class AbstractRegelManuellOulTest extends AbstractRegelManuellTe
       var oulStatusMessage = oulKafkaConnector.waitForOulStatusMessage();
       Assertions.assertEquals(uppgiftId, oulStatusMessage.getUppgiftId());
       Assertions.assertEquals(Status.AVSLUTAD, oulStatusMessage.getStatus());
+   }
+
+   @ParameterizedTest
+   @CsvSource(
+   {
+         "5367f6b8-cc4a-11f0-8de9-199901011234, 11e53b18-e9ac-4707-825b-a1cb80689c29, Idtyp_typId, Idtyp_varde"
+   })
+   void oul_status_should_put_handlaggning_with_status_new(
+         String handlaggningId,
+         String uppgiftId,
+         String idtypTypId,
+         String idtypVarde) throws JsonProcessingException
+   {
+      regelKafkaConnector.sendRegelRequest(handlaggningId);
+      //
+      // mock status update from OUL
+      //
+      var utforarId = ImmutableIdtyp.builder()
+            .typId(idtypTypId)
+            .varde(idtypVarde)
+            .build();
+      oulKafkaConnector.simulateOulStatus(handlaggningId, uppgiftId, utforarId, Status.NY);
+      //
+      // verify PUT handlaggning
+      //
+      var handlaggningPutUpdate = WireMockRegelManuell.getLastPutHandlaggning(handlaggningId);
+      assertEquals(handlaggningId, handlaggningPutUpdate.getHandlaggning().getId().toString());
+      assertEquals(1, handlaggningPutUpdate.getHandlaggning().getVersion());
+      assertEquals("1", handlaggningPutUpdate.getHandlaggning().getUppgift().getUppgiftStatus());
+      assertEquals(2, handlaggningPutUpdate.getHandlaggning().getUppgift().getVersion());
    }
 
 }
