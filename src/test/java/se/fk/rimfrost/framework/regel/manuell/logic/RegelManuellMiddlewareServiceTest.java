6169d118d51fd548d5b1f351713d43dd6cbcad51
Nils Elveros <nils.elveros@tromb.com>
2026-05-07 07:43:37 +0200

    fix: add tests for middleware

--- src/test/java/se/fk/rimfrost/framework/regel/manuell/logic/RegelManuellMiddlewareServiceTest.java
+++ src/test/java/se/fk/rimfrost/framework/regel/manuell/logic/RegelManuellMiddlewareServiceTest.java
@@ -3,31 +3,14 @@ package se.fk.rimfrost.framework.regel.manuell.logic;
 import java.util.UUID;
 import io.quarkus.arc.DefaultBean;
 import jakarta.enterprise.context.ApplicationScoped;
+import se.fk.rimfrost.framework.handlaggning.model.Handlaggning;
+import se.fk.rimfrost.framework.handlaggning.model.HandlaggningUpdate;
 import se.fk.rimfrost.framework.regel.Utfall;
 
 @SuppressWarnings("unused")
 @ApplicationScoped
 @DefaultBean
-public class RegelManuellMiddlewareServiceTest extends RegelManuellServiceBase
-      implements RegelManuellMiddlewareServiceInterface<String, String>
+public class RegelManuellMiddlewareServiceTest extends RegelManuellMiddlewareService<String, String>
 {
-
-   @Override
-   public String read(UUID handlaggningId)
-   {
-      return "read";
-   }
-
-   @Override
-   public void update(UUID handlaggningId, String request)
-   {
-      //update
-   }
-
-   @Override
-   public void done(UUID handlaggningId)
-   {
-      sendRegelResponse(handlaggningId, Utfall.JA);
-   }
 
 }
--- src/test/java/se/fk/rimfrost/framework/regel/manuell/logic/RegelManuellTestService.java
+++ src/test/java/se/fk/rimfrost/framework/regel/manuell/logic/RegelManuellTestService.java
@@ -2,32 +2,54 @@ package se.fk.rimfrost.framework.regel.manuell.logic;
 
 import io.quarkus.arc.DefaultBean;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import se.fk.rimfrost.framework.handlaggning.model.Handlaggning;
 import se.fk.rimfrost.framework.handlaggning.model.HandlaggningUpdate;
 import se.fk.rimfrost.framework.handlaggning.model.ImmutableHandlaggningUpdate;
+import se.fk.rimfrost.framework.handlaggning.model.ProduceratResultat;
+import se.fk.rimfrost.framework.regel.Utfall;
+import se.fk.rimfrost.framework.regel.logic.RegelUtils;
+import se.fk.rimfrost.framework.regel.manuell.storage.ManuellRegelCommonDataStorage;
+
+import java.util.ArrayList;
 import java.util.UUID;
+
+import com.networknt.schema.OutputFormat.List;
 
 @SuppressWarnings("unused")
 @ApplicationScoped
 @DefaultBean
-public class RegelManuellTestService implements RegelManuellServiceInterface<String, String>
+public class RegelManuellTestService extends RegelManuellServiceBase
+      implements RegelManuellServiceInterface<String, String>
 {
+   @Inject
+   ManuellRegelCommonDataStorage dataStorage;
 
    @Override
    public String readData(Handlaggning handlaggning)
    {
-      return "";
-   }
-
-   @Override
-   public HandlaggningUpdate updateData(Handlaggning handlaggning, String request)
-   {
-      return ImmutableHandlaggningUpdate.builder().build();
+      return "read";
    }
 
    @Override
    public void done(UUID handlaggningId)
    {
-      //nope
+      sendRegelResponse(handlaggningId, Utfall.JA);
    }
+
+   @Override
+   public HandlaggningUpdate updateData(Handlaggning handlaggning, String request)
+   {
+      return ImmutableHandlaggningUpdate.builder()
+            .id(handlaggning.id())
+            .version(handlaggning.version())
+            .yrkande(handlaggning.yrkande())
+            .processInstansId(handlaggning.processInstansId())
+            .skapadTS(handlaggning.skapadTS())
+            .avslutadTS(handlaggning.avslutadTS())
+            .handlaggningspecifikationId(handlaggning.handlaggningspecifikationId())
+            .uppgift(dataStorage.getManuellRegelCommonData(handlaggning.id()).uppgift())
+            .build();
+   }
+
 }
